14a8712aa36569ed4901b5c3a84e3546dc31ab00
LCoffe <leonardo.021103@alunos.utfpr.edu.br>
2024-12-07 09:50:18 -0300

    Att import

--- hc05.py
+++ hc05.py
@@ -2,36 +2,33 @@ from pyfirmata import Arduino # type: ignore
 import cv2
 import time
 import tkinter as tk
+import rcf
 
-webcam = cv2.VideoCapture(0)
+#webcam = cv2.VideoCapture(0)
 
-if(not webcam.isOpened()):
-    print("Error: Could not open webcam.")
+#if(not webcam.isOpened()):
+    #print("Error: Could not open webcam.")
 
 window = tk.Tk()
 window.configure(bg='black')
 window.geometry("500x500")
 window.title("Bluetooth Control")
 
-board = Arduino('COM7')
+#board = Arduino('COM7')
 
-pin_5 = board.get_pin('d:5:p')
-pin_6 = board.get_pin('d:6:p')
+#pin_5 = board.get_pin('d:5:p')
+#pin_6 = board.get_pin('d:6:p')
 
 def motor_on():
-    pin_5.write(0.1)
-    pin_6.write(0)
+    #pin_5.write(0.1)
+    #pin_6.write(0)
     print("Motor ON")
 
 def motor_off():
-    pin_5.write(0)
-    pin_6.write(0)
+    #pin_5.write(0)
+    #pin_6.write(0)
 
-    valid, frame = webcam.read()
-    if valid:
-        cv2.imshow("Webcam", frame)
-        cv2.waitKey(0)
-        cv2.destroyAllWindows()
+    print(rcf.main()) # Função principal para reconhecimento.
 
     print("Motor OFF")
 
