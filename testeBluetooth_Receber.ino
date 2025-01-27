700bcba28295d49b5fc3f3055e219b6f844f4292
LCoffe <leonardo.021103@alunos.utfpr.edu.br>
2025-01-27 18:37:32 -0300

    att teste

--- testeBluetooth_Receber.ino
+++ testeBluetooth_Receber.ino
@@ -1,26 +1,49 @@
+#include <SoftwareSrial.h>
+#include <Servo.h>
+
+int motorPlus = 5;
+int motorMinus = 6;
+int rxPin = 10;
+int txPin = 11;
+SoftwareSerial mySerial(rxPin, txPin);
+Servo myServo;
 void setup() {
   // Inicializa a comunicação serial
   Serial.begin(9600);
+  mySerial.begin(9600);
+  myServo.attach(9);
+  pinMode(motorPlus, OUTPUT);
+  pinMode(motorMinus, OUTPUT);
 }
 
 void loop() {
-  int i = 0;
-  while(i > 60){
-    delay(1000);
-    i++;
-  }
+  if (Serial.available() > 0 && mySerial.available() > 0) {  // Verifica se há dados recebidos na porta serial
+    while(mySerial.read() == '1'){ //Caso saia do while e por que esta off o aplicativo bluetooth 
+      delay(30000);
+      Serial.println("1");
 
-  Serial.println("1"); //Envia 1 na porta serial para o python ler
-
-  if (Serial.available() > 0) {  // Verifica se há dados recebidos na porta serial
-    String receivedData = Serial.readStringUntil('\n');  // Lê até o caractere de nova linha '\n'
-    int receivedInt = receivedData.toInt();  // Converte a String para inteiro
-    if(receivedInt == 0){
-      Serial.println("Achou algo diferente de limão e laranja");
-    } else if(receivedInt < 180){
-      Serial.println("Laranja");
-    } else {
-      Serial.println("Limão");
+      String receivedData = Serial.readStringUntil('\n');  // Lê até o caractere de nova linha '\n'
+      int receivedInt = receivedData.toInt();  // Converte a String para inteiro
+  
+      if(receivedInt == 0){
+        Serial.println("Achou algo diferente de limão e laranja");
+        analogWrite(motorPlus, 0);
+        analogWrite(motorMinus, 128);
+      } else if(receivedInt < 180){
+        Serial.println("Laranja");
+        myServo.write(40;
+        analogWrite(motorPlus, 128);
+        analogWrite(motorMinus, 0);
+        delay(1000);
+      } else {
+        Serial.println("Limão");
+        myServo.write(140);
+        analogWrite(motorPlus, 128);
+        analogWrite(motorMinus, 0);
+        delay(1000);
+      }
     }
   }
+  analogWrite(motorPlus, 0);
+  analogWrite(motorMinus, 0);
 }
--- teste_bluetooth.py
+++ teste_bluetooth.py
@@ -3,19 +3,19 @@ import time
 import rcfAuto as rcf
 
 # Configura a porta serial (verifique a porta correta onde o Arduino está conectado)
-arduino = serial.Serial('COM6', 9600)  # Substitua 'COMx' pela sua porta, ex: 'COM3' no Windows ou '/dev/ttyUSB0' no Linux/Mac
+bluetooth = serial.Serial('COM6', 9600)  # Substitua 'COMx' pela sua porta, ex: 'COM3' no Windows ou '/dev/ttyUSB0' no Linux/Mac
 time.sleep(2)  # Espera o Arduino inicializar
 
 # Função para enviar dados para o Arduino
 def enviar_para_arduino(dado):
-    arduino.write(str(dado).encode())  # Converte o dado para string e envia via serial
+    bluetooth.write(str(dado).encode())  # Converte o dado para string e envia via serial
     print(f'Enviado para o Arduino: {dado}')
-    arduino.write(b'\n')  # Envia um caractere de nova linha para indicar o final
+    bluetooth.write(b'\n')  # Envia um caractere de nova linha para indicar o final
 
 # Função para receber dados do Arduino
 def receber_do_arduino():
-    if arduino.in_waiting > 0:  # Verifica se há dados disponíveis para leitura
-        linha = arduino.readline().decode('utf-8').strip()  # Lê uma linha de dados e decodifica
+    if bluetooth.in_waiting > 0:  # Verifica se há dados disponíveis para leitura
+        linha = bluetooth.readline().decode('utf-8').strip()  # Lê uma linha de dados e decodifica
         print(f'Recebido do Arduino: {linha}')
         try:
             return int(linha)  # Converte a linha para inteiro e retorna
@@ -24,9 +24,11 @@ def receber_do_arduino():
 
 # Envia um número inteiro para o Arduino
 while True:
-    num = receber_do_arduino  # O número que você quer enviar para o Arduino
-
+    num = receber_do_arduino()  # Recebe um número inteiro do Arduino
+    #num = int(input('Digite 1 para iniciar o teste: '))
     # Envia o número inteiro para o Arduino
     if(num == 1):
         aux = rcf.main()
         enviar_para_arduino(aux)
+
+    
